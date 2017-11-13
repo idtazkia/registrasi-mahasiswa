c683d3107c047f7d7995741ed4adc81aca8717cb
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-13 22:33:11 +0700

    add default page

---
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/HomeController.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.registration.registrasimahasiswa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String defaultPage(){
+        return "redirect:/registrasi/pendaftar";
+    }
+}
