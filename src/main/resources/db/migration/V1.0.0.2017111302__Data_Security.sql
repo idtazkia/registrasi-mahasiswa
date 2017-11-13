9de04deee311205db73f51065fc825a0155c615f
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-13 22:20:47 +0700

    fix migration script

--- src/main/resources/db/migration/V1.0.0.2017111302__Data_Security.sql
+++ src/main/resources/db/migration/V1.0.0.2017111302__Data_Security.sql
@@ -1,16 +1,6 @@
-INSERT INTO s_permission (id, permission_value, permission_label) VALUES
-  ('editmaster', 'EDIT_MASTER', 'Edit Master'),
-  ('viewmaster', 'VIEW_MASTER', 'View Master');
-
 INSERT INTO s_role (id, description, name) VALUES
   ('user', 'USER', 'User'),
   ('admin', 'ADMIN', 'Admin');
-
-INSERT INTO s_role_permission (id_role, id_permission) VALUES
-  ('user', 'viewpendaftaran'),
-  ('user', 'editpendaftaran'),
-  ('admin', 'viewmaster'),
-  ('admin', 'editmaster');
 
 INSERT INTO s_user (id, active, username, id_role) VALUES
   ('u001', true, 'user', 'user');
@@ -23,6 +13,3 @@ INSERT INTO s_user_password (id_user, password) VALUES
 
 INSERT INTO s_user_password (id_user, password) VALUES
   ('u002', '1234');
-
--- "SELECT u.username,ur.name AS authority FROM s_user u JOIN s_role ur ON  u.id_role = ur.id\n" +
--- "where u.username = ?";
