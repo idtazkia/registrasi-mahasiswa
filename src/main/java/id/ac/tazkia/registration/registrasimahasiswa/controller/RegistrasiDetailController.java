19ede1bdb5451bae4dcf335a386917529207aa43
Haffizh <haffizh@tazkia.ac.id>
2017-11-20 11:36:42 +0700

    memanggil value nama dari pendaftar

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/RegistrasiDetailController.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/RegistrasiDetailController.java
@@ -2,9 +2,16 @@ package id.ac.tazkia.registration.registrasimahasiswa.controller;
 
 import id.ac.tazkia.registration.registrasimahasiswa.dao.DetailPendaftarDao;
 import id.ac.tazkia.registration.registrasimahasiswa.dao.PendaftarDao;
+import id.ac.tazkia.registration.registrasimahasiswa.dao.UserDao;
+import id.ac.tazkia.registration.registrasimahasiswa.dto.RegistrasiDetail;
 import id.ac.tazkia.registration.registrasimahasiswa.entity.DetailPendaftar;
 import id.ac.tazkia.registration.registrasimahasiswa.entity.Pendaftar;
+import id.ac.tazkia.registration.registrasimahasiswa.entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,7 +24,10 @@ import javax.validation.Valid;
 @Controller
 public class RegistrasiDetailController {
 
+    private static final Logger logger = LoggerFactory.getLogger(RegistrasiDetailController.class);
+
     @Autowired private PendaftarDao pendaftarDao;
+    @Autowired private UserDao userDao;
 
     @Autowired
     private DetailPendaftarDao detailPendaftarDao;
@@ -28,12 +38,33 @@ public class RegistrasiDetailController {
     }
 
 
-    @GetMapping(value = "/registrasi/{id}/form")
-    public String tampilkanForm(Model model, @PathVariable String id){
-        Pendaftar p = pendaftarDao.findOne(id);
-        DetailPendaftar dp = new DetailPendaftar();
-        dp.setPendaftar(p);
-        model.addAttribute("registrasi", dp);
+    @GetMapping(value = "/registrasi/detail/form")
+    public String tampilkanForm(Model model, Authentication currentUser){
+        logger.debug("Authentication class : {}",currentUser.getClass().getName());
+
+        if(currentUser == null){
+            return "/registrasi/detail/form";
+        }
+
+        User u = userDao.findByUsername(((UserDetails)currentUser.getPrincipal()).getUsername());
+        logger.debug("User ID : {}", u.getId());
+        if(u == null){
+            return "/registrasi/detail/form";
+        }
+
+
+
+        Pendaftar p = pendaftarDao.findByUser(u);
+        logger.debug("Nama Pendaftar : "+p.getNama());
+        if(p == null){
+            return "/registrasi/detail/form";
+        }
+
+        RegistrasiDetail detail = new RegistrasiDetail();
+        detail.setNama(p.getNama());
+        detail.setAsalSekolah(p.getNamaAsalSekolah());
+        detail.setKabupatenKota(p.getKabupatenKota().getNama());
+        model.addAttribute("registrasi", detail);
         return "/registrasi/detail/form";
     }
 
