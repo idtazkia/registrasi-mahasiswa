469f5d9e0a5069bc5358c001d2e843bddbb22967
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-21 13:36:06 +0700

    fix import

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/dao/UserPasswordDao.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/dao/UserPasswordDao.java
@@ -1,6 +1,5 @@
 package id.ac.tazkia.registration.registrasimahasiswa.dao;
 
-import id.ac.tazkia.registration.registrasimahasiswa.entity.User;
 import id.ac.tazkia.registration.registrasimahasiswa.entity.UserPassword;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
