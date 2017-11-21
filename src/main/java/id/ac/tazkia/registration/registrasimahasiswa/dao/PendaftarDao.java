5eb647aa1fc135e963e44ff0dbf3b24074d7ab23
Haffizh <haffizh@tazkia.ac.id>
2017-11-21 09:59:57 +0700

    peserta dao

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/dao/PendaftarDao.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/dao/PendaftarDao.java
@@ -1,11 +1,12 @@
 package id.ac.tazkia.registration.registrasimahasiswa.dao;
 
 import id.ac.tazkia.registration.registrasimahasiswa.entity.Pendaftar;
+import id.ac.tazkia.registration.registrasimahasiswa.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface PendaftarDao extends PagingAndSortingRepository<Pendaftar, String> {
     Page<Pendaftar> findByNamaContainingIgnoreCaseOrderByNama(String nama, Pageable page);
-
+    Pendaftar findByUser(User u);
 }
