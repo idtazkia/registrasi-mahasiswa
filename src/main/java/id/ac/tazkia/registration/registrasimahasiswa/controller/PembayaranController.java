dfd7f922d130662e26068d2717f170fcd24c3acb
Haffizh <haffizh@tazkia.ac.id>
2017-11-23 13:34:47 +0700

    controller verifikasi

---
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/PembayaranController.java
@@ -0,0 +1,11 @@
+package id.ac.tazkia.registration.registrasimahasiswa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PembayaranController {
+
+    @GetMapping("/biaya/pembayaran/list")
+    public void form(){}
+}
