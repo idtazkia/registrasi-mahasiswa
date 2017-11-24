c516c27b7a7f5f088f6d9b2fba8b3d4a2af3ab80
Haffizh <haffizh@tazkia.ac.id>
2017-11-24 13:56:57 +0700

    screen input tagihan

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/TagihanController.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/TagihanController.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PembayaranController {
+public class TagihanController {
 
     @GetMapping("/biaya/tagihan/list")
-    public void form(){}
+    public void listTagihan(){}
+
+    @GetMapping("/biaya/tagihan/form")
+    public  void formTagihan(){}
 }
