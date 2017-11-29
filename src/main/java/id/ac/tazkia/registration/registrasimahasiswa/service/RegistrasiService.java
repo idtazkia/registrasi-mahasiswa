630b9931642469f462a584da1fc0ea90f2861fe5
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-29 19:15:54 +0700

    delete import yang tidak perlu

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/service/RegistrasiService.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/service/RegistrasiService.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
