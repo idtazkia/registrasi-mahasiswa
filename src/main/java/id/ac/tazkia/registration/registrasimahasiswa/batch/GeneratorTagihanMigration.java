29a52787854898fc120e0fc7969905450d672029
Endy Muhardin <endy.muhardin@gmail.com>
2017-11-29 19:58:49 +0700

    disable migrator tagihan pendaftaran

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/batch/GeneratorTagihanMigration.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/batch/GeneratorTagihanMigration.java
@@ -9,9 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
-@Component
 public class GeneratorTagihanMigration implements CommandLineRunner{
     private static final Logger LOGGER = LoggerFactory.getLogger(GeneratorTagihanMigration.class);
 
