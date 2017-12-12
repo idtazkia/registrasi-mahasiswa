da74cdc3fc88dd5b55dd322e000636d198439152
Haffizh <haffizh@tazkia.ac.id>
2017-12-12 16:08:12 +0700

    Regsitrasi Detail Simpan dan Edit

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/dao/DetailPendaftarDao.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/dao/DetailPendaftarDao.java
@@ -1,8 +1,10 @@
 package id.ac.tazkia.registration.registrasimahasiswa.dao;
 
 import id.ac.tazkia.registration.registrasimahasiswa.entity.DetailPendaftar;
+import id.ac.tazkia.registration.registrasimahasiswa.entity.Pendaftar;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface DetailPendaftarDao extends PagingAndSortingRepository<DetailPendaftar, String> {
 
+    DetailPendaftar findByPendaftar(Pendaftar p);
 }
