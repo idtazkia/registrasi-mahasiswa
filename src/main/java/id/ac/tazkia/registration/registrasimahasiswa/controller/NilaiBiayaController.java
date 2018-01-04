cd389e255393542dbd2920b3ce6c69873fa25c64
Haffizh <haffizh@tazkia.ac.id>
2018-01-04 10:35:26 +0700

    perbaikan menu grade dan menu nilai jenis biaya

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/NilaiBiayaController.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/NilaiBiayaController.java
@@ -2,16 +2,24 @@ package id.ac.tazkia.registration.registrasimahasiswa.controller;
 
 import id.ac.tazkia.registration.registrasimahasiswa.dao.NilaiBiayaDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class NilaiBiayaController {
     @Autowired private NilaiBiayaDao nb;
 
     @GetMapping("/biaya/nilai/list")
-    public void daftarNilaiBiaya(Model m){
-        m.addAttribute("daftarNilai", nb.findAll());
+    public void daftarNilaiBiaya(@RequestParam(required = false)String nama, Model m, Pageable page){
+        if(StringUtils.hasText(nama)) {
+            m.addAttribute("nama", nama);
+            m.addAttribute("daftarNilai", nb.findByJenisBiayaContainingIgnoreCaseOrderByJenisBiaya(nama, page));
+        } else {
+            m.addAttribute("daftarNilai", nb.findAll(page));
+        }
     }
 }
--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/dao/NilaiBiayaDao.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/dao/NilaiBiayaDao.java
@@ -8,4 +8,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface NilaiBiayaDao extends PagingAndSortingRepository <NilaiBiaya, String> {
     Page<NilaiBiaya> findByJenisBiaya(JenisBiaya jd, Pageable page);
+
+    Page<JenisBiaya> findByJenisBiayaContainingIgnoreCaseOrderByJenisBiaya(String nama, Pageable page);
 }
