9d1db249fbaef84930c734a12404679bb261fe8b
Haffizh <haffizh@tazkia.ac.id>
2018-02-09 13:37:37 +0700

    Penambahan jenisTest & hapus skema smart test

---
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/entity/JenisTest.java
@@ -0,0 +1,7 @@
+package id.ac.tazkia.registration.registrasimahasiswa.entity;
+
+public enum JenisTest {
+    SMART_TEST,
+    JPA,
+    TPA
+}
--- src/main/resources/db/migration/V1.3.0.2018020101__Skema_Smart_Test.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE smart_test (
-  id                VARCHAR (36),
-  nama              VARCHAR(255) NOT NULL,
-  sekolah           VARCHAR(255),
-  no_hp             VARCHAR(255),
-  nilai             VARCHAR(255),
-  PRIMARY KEY (id)
-);
