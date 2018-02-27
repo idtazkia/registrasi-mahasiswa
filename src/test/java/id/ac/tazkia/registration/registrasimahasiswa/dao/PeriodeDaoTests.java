a076c6e4cdc1f65dd99d4657917dfcddcb0e7828
Haffizh <haffizh@tazkia.ac.id>
2018-02-27 12:53:35 +0700

    Perbaikan format date

--- src/test/java/id/ac/tazkia/registration/registrasimahasiswa/dao/PeriodeDaoTests.java
+++ src/test/java/id/ac/tazkia/registration/registrasimahasiswa/dao/PeriodeDaoTests.java
@@ -1,6 +1,5 @@
 package id.ac.tazkia.registration.registrasimahasiswa.dao;
 
-import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import id.ac.tazkia.registration.registrasimahasiswa.entity.Periode;
 import org.junit.Assert;
 import org.junit.Test;
@@ -9,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -23,11 +19,8 @@ public class PeriodeDaoTests {
 
     @Test
     public void testCariPeriodeByTanggalTest() throws  Exception{
-        DateTimeFormatter formatterTanggal = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate tanggalPeriode1 = LocalDate.parse("2018-01-01", formatterTanggal);
-        LocalDate tanggalPeriode2 = LocalDate.parse("2018-05-01", formatterTanggal);
-        LocalDate tanggalPeriode3 = LocalDate.parse("2018-10-31", formatterTanggal);
-        LocalDate tanggalPeriodeInvalid = (LocalDate) formatterTanggal.parse("2019-01-01");
+        LocalDate tanggalPeriode1 = LocalDate.parse("2018-01-01");
+        LocalDate tanggalPeriodeInvalid = LocalDate.parse("2019-01-01");
 
         Assert.assertTrue(periodeDao.cariPeriodeUntukTanggal(tanggalPeriodeInvalid).isEmpty());
         List<Periode> daftarPeriode1 = periodeDao.cariPeriodeUntukTanggal(tanggalPeriode1);
