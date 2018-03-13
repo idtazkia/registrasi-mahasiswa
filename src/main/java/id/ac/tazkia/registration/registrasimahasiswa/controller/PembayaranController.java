32323b8964ff8baa5d4536227c2cb4101ab15f44
Haffizh <haffizh@tazkia.ac.id>
2018-03-13 13:07:24 +0700

    form nirm

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/PembayaranController.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/PembayaranController.java
@@ -22,6 +22,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -55,10 +56,11 @@ public class PembayaranController {
     private UserDao userDao;
     @Value("${upload.folder}")
     private String uploadFolder;
-    @Autowired private RegistrasiService registrasiService;
+    @Autowired
+    private RegistrasiService registrasiService;
 
     @ModelAttribute("daftarBank")
-    public Iterable<Bank> daftarBank(){
+    public Iterable<Bank> daftarBank() {
         return bankDao.findAll();
     }
 
@@ -71,14 +73,14 @@ public class PembayaranController {
     @RequestMapping(value = "/biaya/pembayaran/form", method = RequestMethod.GET)
     public void tampilkanForm(@RequestParam(value = "id", required = true) String id,
                               @RequestParam(required = false) String error,
-                              Model m){
+                              Model m) {
         //defaultnya, isi dengan object baru
         Pembayaran p = new Pembayaran();
         m.addAttribute("bayar", p);
         m.addAttribute("error", error);
 
         Tagihan t = tagihanDao.findOne(id);
-        if (t != null){
+        if (t != null) {
             p.setNilai(t.getNilai());
             p.setTagihan(t);
         }
@@ -86,11 +88,11 @@ public class PembayaranController {
 
     @RequestMapping(value = "/biaya/pembayaran/form", method = RequestMethod.POST)
     public String prosesForm(@ModelAttribute @Valid Pembayaran pembayaran, BindingResult errors,
-                             MultipartFile fileBukti) throws Exception{
+                             MultipartFile fileBukti) throws Exception {
 
         if (errors.hasErrors()) {
-            LOGGER.debug("Error upload bukti pembayaran : {}",errors.toString());
-            return "redirect:/biaya/pembayaran/form?error=Invalid&id="+pembayaran.getTagihan().getId();
+            LOGGER.debug("Error upload bukti pembayaran : {}", errors.toString());
+            return "redirect:/biaya/pembayaran/form?error=Invalid&id=" + pembayaran.getTagihan().getId();
         }
 
         String idPeserta = pembayaran.getTagihan().getPendaftar().getId();
@@ -112,17 +114,17 @@ public class PembayaranController {
         int p = Math.max(namaAsli.lastIndexOf('/'), namaAsli.lastIndexOf('\\'));
 
         if (i > p) {
-            extension = namaAsli.substring(i+1);
+            extension = namaAsli.substring(i + 1);
         }
 
         String idFile = UUID.randomUUID().toString();
-        String lokasiUpload = uploadFolder+ File.separator + idPeserta;
-        LOGGER.debug("Lokasi upload : {}",lokasiUpload);
+        String lokasiUpload = uploadFolder + File.separator + idPeserta;
+        LOGGER.debug("Lokasi upload : {}", lokasiUpload);
         new File(lokasiUpload).mkdirs();
         File tujuan = new File(lokasiUpload + File.separator + idFile + "." + extension);
-        pembayaran.setBuktiPembayaran(idFile+"."+extension);
+        pembayaran.setBuktiPembayaran(idFile + "." + extension);
         fileBukti.transferTo(tujuan);
-        LOGGER.debug("File sudah dicopy ke : {}",tujuan.getAbsolutePath());
+        LOGGER.debug("File sudah dicopy ke : {}", tujuan.getAbsolutePath());
 
         Tagihan tagihan = pembayaran.getTagihan();
         tagihan.setLunas(true);
@@ -130,15 +132,15 @@ public class PembayaranController {
 
         registrasiService.aktivasiUser(tagihan.getPendaftar());
 
-        LOGGER.debug("Bank : {}",pembayaran.getBank());
+        LOGGER.debug("Bank : {}", pembayaran.getBank());
 
         pembayaranDao.save(pembayaran);
         return "redirect:/registrasi/list";
     }
 
     @RequestMapping("/biaya/pembayaran/list")
-    public void  list(@RequestParam(value = "id",required = false)String idTagihan, Model m, Pageable page){
-        if(StringUtils.hasText(idTagihan)) {
+    public void list(@RequestParam(value = "id", required = false) String idTagihan, Model m, Pageable page) {
+        if (StringUtils.hasText(idTagihan)) {
             m.addAttribute("nama", idTagihan);
             Tagihan t = tagihanDao.findOne(idTagihan);
             m.addAttribute("pembayaran", pembayaranDao.findByTagihan(t));
@@ -150,34 +152,34 @@ public class PembayaranController {
 
 
     @GetMapping("/pembayaran/{pembayaran}/bukti/")
-    public ResponseEntity<byte[]> tampilkanBuktiPembayaran(@PathVariable Pembayaran pembayaran) throws Exception{
+    public ResponseEntity<byte[]> tampilkanBuktiPembayaran(@PathVariable Pembayaran pembayaran) throws Exception {
         String lokasiFile = uploadFolder + File.separator + pembayaran.getTagihan().getPendaftar().getId()
                 + File.separator + pembayaran.getBuktiPembayaran();
-        LOGGER.debug("Lokasi file bukti : {}",lokasiFile);
+        LOGGER.debug("Lokasi file bukti : {}", lokasiFile);
 
         try {
             HttpHeaders headers = new HttpHeaders();
-            if(pembayaran.getBuktiPembayaran().toLowerCase().endsWith("jpeg") || pembayaran.getBuktiPembayaran().toLowerCase().endsWith("jpg")) {
+            if (pembayaran.getBuktiPembayaran().toLowerCase().endsWith("jpeg") || pembayaran.getBuktiPembayaran().toLowerCase().endsWith("jpg")) {
                 headers.setContentType(MediaType.IMAGE_JPEG);
-            } else if(pembayaran.getBuktiPembayaran().toLowerCase().endsWith("png")){
+            } else if (pembayaran.getBuktiPembayaran().toLowerCase().endsWith("png")) {
                 headers.setContentType(MediaType.IMAGE_PNG);
             } else {
                 headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
             }
             byte[] data = Files.readAllBytes(Paths.get(lokasiFile));
             return new ResponseEntity<byte[]>(data, headers, HttpStatus.OK);
-        } catch (Exception err){
+        } catch (Exception err) {
             LOGGER.warn(err.getMessage(), err);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
-//list
+    //list
     @GetMapping("/registrasi/pembayaran/list")
-    public void daftarPembayaran(@RequestParam(required = false)String pendaftar, Model m, Pageable page){
-        if(StringUtils.hasText(pendaftar)) {
+    public void daftarPembayaran(@RequestParam(required = false) String pendaftar, Model m, Pageable page) {
+        if (StringUtils.hasText(pendaftar)) {
             m.addAttribute("pendaftar", pendaftar);
-            m.addAttribute("daftarPembayaran", pembayaranDao.cariByPendaftar("%"+pendaftar.toLowerCase()+"%", page));
+            m.addAttribute("daftarPembayaran", pembayaranDao.cariByPendaftar("%" + pendaftar.toLowerCase() + "%", page));
         } else {
             m.addAttribute("daftarPembayaran", pembayaranDao.findAll(page));
         }
@@ -188,7 +190,7 @@ public class PembayaranController {
 
     @GetMapping("/suratKeterangan")
     public void suratKeterangan(@RequestParam(name = "id") Pendaftar pendaftar,
-                           HttpServletResponse response){
+                                HttpServletResponse response) {
         try {
             // 0. Setup converter
             Options options = Options.getFrom(DocumentKind.ODT).to(ConverterTypeTo.PDF);
@@ -226,9 +228,22 @@ public class PembayaranController {
             OutputStream out = response.getOutputStream();
             report.convert(ctx, options, out);
             out.flush();
-        } catch (Exception err){
+        } catch (Exception err) {
             LOGGER.error(err.getMessage(), err);
         }
+    }
+
+    //
+//FORM NIMKO
+    @Value("classpath:sample/Form NIRM.xlsx")
+    private Resource formNimko;
+
+    @GetMapping("/formNimko")
+    public void downloadContohFileTagihan(HttpServletResponse response) throws Exception {
+        response.setContentType("text/xlsx");
+        response.setHeader("Content-Disposition", "attachment; filename=Form NIRM.xlsx");
+        FileCopyUtils.copy(formNimko.getInputStream(), response.getOutputStream());
+        response.getOutputStream().flush();
     }
 //
 
