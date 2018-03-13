09b49ae781140052078c643f4bcf86b426db61f6
Haffizh <haffizh@tazkia.ac.id>
2018-03-13 10:58:56 +0700

    surat keteranagn lulus

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/PembayaranController.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/PembayaranController.java
@@ -1,17 +1,20 @@
 package id.ac.tazkia.registration.registrasimahasiswa.controller;
 
-import id.ac.tazkia.registration.registrasimahasiswa.dao.BankDao;
-import id.ac.tazkia.registration.registrasimahasiswa.dao.PembayaranDao;
-import id.ac.tazkia.registration.registrasimahasiswa.dao.TagihanDao;
-import id.ac.tazkia.registration.registrasimahasiswa.dao.UserDao;
-import id.ac.tazkia.registration.registrasimahasiswa.entity.Bank;
-import id.ac.tazkia.registration.registrasimahasiswa.entity.Pembayaran;
-import id.ac.tazkia.registration.registrasimahasiswa.entity.Tagihan;
+import fr.opensagres.xdocreport.converter.ConverterTypeTo;
+import fr.opensagres.xdocreport.converter.Options;
+import fr.opensagres.xdocreport.core.document.DocumentKind;
+import fr.opensagres.xdocreport.document.IXDocReport;
+import fr.opensagres.xdocreport.document.registry.XDocReportRegistry;
+import fr.opensagres.xdocreport.template.IContext;
+import fr.opensagres.xdocreport.template.TemplateEngineKind;
+import id.ac.tazkia.registration.registrasimahasiswa.dao.*;
+import id.ac.tazkia.registration.registrasimahasiswa.entity.*;
 import id.ac.tazkia.registration.registrasimahasiswa.service.RegistrasiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -24,10 +27,17 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.UUID;
 
 @Controller
@@ -51,6 +61,12 @@ public class PembayaranController {
     public Iterable<Bank> daftarBank(){
         return bankDao.findAll();
     }
+
+    @Value("classpath:keterangan-lulus.odt")
+    private Resource templateSuratKeterangan;
+
+    @Autowired
+    private DetailPendaftarDao detailPendaftarDao;
 
     @RequestMapping(value = "/biaya/pembayaran/form", method = RequestMethod.GET)
     public void tampilkanForm(@RequestParam(value = "id", required = true) String id,
@@ -167,5 +183,54 @@ public class PembayaranController {
         }
     }
 /////
+
+//Surat keterangan
+
+    @GetMapping("/suratKeterangan")
+    public void suratKeterangan(@RequestParam(name = "id") Pendaftar pendaftar,
+                           HttpServletResponse response){
+        try {
+            // 0. Setup converter
+            Options options = Options.getFrom(DocumentKind.ODT).to(ConverterTypeTo.PDF);
+
+            // 1. Load template dari file
+            InputStream in = templateSuratKeterangan.getInputStream();
+
+            // 2. Inisialisasi template engine, menentukan sintaks penulisan variabel
+            IXDocReport report = XDocReportRegistry.getRegistry().
+                    loadReport(in, TemplateEngineKind.Freemarker);
+
+            // 3. Context object, untuk mengisi variabel
+            DetailPendaftar d = detailPendaftarDao.findByPendaftar(pendaftar);
+
+            IContext ctx = report.createContext();
+            ctx.put("nama", d.getPendaftar().getNama());
+            ctx.put("ttl", d.getTtl());
+            ctx.put("asalSekolah", d.getAsalSekolah());
+            ctx.put("namaAyah", d.getNamaAyah());
+            ctx.put("alamat", d.getAlamatRumah());
+            ctx.put("programStudi", d.getPendaftar().getProgramStudi().getNama());
+
+            Locale indonesia = new Locale("id", "id");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy", indonesia);
+
+            LocalDate tanggal =
+                    LocalDate.now(ZoneId.systemDefault());
+
+
+            String tanggalSekarang = tanggal.format(formatter);
+
+            ctx.put("tanggalSekarang", tanggalSekarang);
+
+            response.setHeader("Content-Disposition", "attachment;filename=Surat-Keterangan.pdf");
+            OutputStream out = response.getOutputStream();
+            report.convert(ctx, options, out);
+            out.flush();
+        } catch (Exception err){
+            LOGGER.error(err.getMessage(), err);
+        }
+    }
+//
+
 }
 
