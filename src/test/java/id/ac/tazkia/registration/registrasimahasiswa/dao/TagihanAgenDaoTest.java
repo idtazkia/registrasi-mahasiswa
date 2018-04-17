f489f262c0a4cc4d7b42b337a4317a15bc48b7e5
Haffizh <haffizh@tazkia.ac.id>
2018-04-17 14:11:03 +0700

    TagihanAgenDao Test

--- src/test/java/id/ac/tazkia/registration/registrasimahasiswa/dao/TagihanAgenDaoTest.java
+++ src/test/java/id/ac/tazkia/registration/registrasimahasiswa/dao/TagihanAgenDaoTest.java
@@ -1,16 +1,16 @@
 package id.ac.tazkia.registration.registrasimahasiswa.dao;
 
 
-import id.ac.tazkia.registration.registrasimahasiswa.dto.RekapTagihanAgen;
 import id.ac.tazkia.registration.registrasimahasiswa.entity.Agen;
+import id.ac.tazkia.registration.registrasimahasiswa.entity.StatusTagihan;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,14 +21,15 @@ public class TagihanAgenDaoTest {
     @Test
     public void tagihanTest(){
         Agen a = new Agen();
-        a.setId("a154eabe-9c95-43a2-adc1-46818d1c6f1a");
+        a.setId("0e5642d8-421a-4c53-9a16-b45755f6d7a3");
         LocalDateTime mulai = LocalDateTime.of(2018,4,1,0,0,0);
         LocalDateTime sampai = LocalDateTime.now();
-        List<RekapTagihanAgen> hasil = pendaftarAgenDao.rekapTagihan(a, mulai, sampai);
-        for (RekapTagihanAgen r : hasil) {
-            System.out.println("Tanggal : "+r.getTanggal());
-            System.out.println("Jumlah : "+r.getJumlah());
-            System.out.println("Nilai : "+r.getNilai());
+        Long jumlahPendaftar = pendaftarAgenDao.jumlahTagihanAgen(a, mulai, sampai, StatusTagihan.BELUM_DITAGIH);
+        BigDecimal nilaiTagihan =
+                new BigDecimal(jumlahPendaftar)
+                        .multiply(BigDecimal.valueOf(200000));
+        System.out.println("Agen :" + a.getNamaCabang() );
+        System.out.println("Jumlah Pendaftar : "+ jumlahPendaftar);
+        System.out.println("Nilai Tagihan :" + nilaiTagihan);
         }
-    }
 }
