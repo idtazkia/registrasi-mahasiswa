f1031860a7aa604406337f7b797f56ca0774d0bd
Endy Muhardin <endy.muhardin@gmail.com>
2018-04-24 15:16:17 +0700

    update skema agen

---
+++ src/main/resources/db/migration/V1.14.2.2018042401__Update_Skema_Agen.sql
@@ -0,0 +1,45 @@
+-- Tbl Pendaftar_Agen
+CREATE TABLE pendaftar_agen (
+  id       VARCHAR (36),
+  id_agen VARCHAR (36) NOT NULL,
+  id_pendaftar VARCHAR (36) NOT NULL,
+  tanggal TIMESTAMP NOT NULL,
+  status_tagihan VARCHAR (36)  NOT NULL,
+  PRIMARY KEY (id),
+  foreign key (id_pendaftar) references pendaftar (id),
+  foreign key (id_agen) references agen (id)
+);
+-- ///
+
+-- Tagihan Agen
+
+CREATE TABLE tagihan_agen (
+  id       VARCHAR (36),
+  id_agen VARCHAR (36) NOT NULL,
+  id_jenisbiaya VARCHAR (36) NOT NULL,
+  nomor_tagihan VARCHAR (255) NOT NULL,
+  tanggal_tagihan DATE NOT NULL,
+  keterangan VARCHAR (255) NOT NULL,
+  nilai NUMERIC (19,2) NOT NULL,
+  lunas BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (id),
+  foreign key (id_agen) references agen (id),
+  foreign key (id_jenisbiaya) references jenis_biaya (id)
+);
+
+--////
+
+-- Pembayaran Agen
+
+create table pembayaran_agen (
+  id VARCHAR (36),
+  id_tagihan VARCHAR(36) NOT NULL,
+  id_bank VARCHAR (36) NOT NULL,
+  waktu_pembayaran TIMESTAMP NOT NULL ,
+  cara_pembayaran VARCHAR (255) NOT NULL ,
+  bukti_pembayaran VARCHAR (255) NOT NULL ,
+  nilai NUMERIC(19,2) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_tagihan) REFERENCES tagihan_agen(id),
+  FOREIGN KEY (id_bank) REFERENCES bank(id)
+);
