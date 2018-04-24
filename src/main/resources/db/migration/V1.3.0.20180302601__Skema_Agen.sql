9a627467d1ffa1a38eb5066f74a3d807dee16d9c
Endy Muhardin <endy.muhardin@gmail.com>
2018-04-24 15:13:49 +0700

    restore file ke posisi awal

--- src/main/resources/db/migration/V1.3.0.20180302601__Skema_Agen.sql
+++ src/main/resources/db/migration/V1.3.0.20180302601__Skema_Agen.sql
@@ -12,69 +12,18 @@ INSERT INTO s_role_permission (id_role, id_permission) VALUES
 
 -- ////
 
-INSERT INTO jenis_biaya VALUES ('010','Agen Pendaftar');
-
 --Tbl Agen
 
 CREATE TABLE agen (
   id       VARCHAR (36),
-  nama_cabang     VARCHAR(255) NOT NULL,
+  nama     VARCHAR(255) NOT NULL,
   alamat_cabang   VARCHAR(255) NOT NULL,
   no_hp     VARCHAR(255) NOT NULL,
   email    VARCHAR(255) NOT NULL,
-  penanggung_jawab    VARCHAR(255) NOT NULL,
-  kode    VARCHAR(255) NOT NULL,
   status   BOOLEAN      NOT NULL,
   id_user VARCHAR (36) NOT NULL,
   PRIMARY KEY (id),
   foreign key (id_user) references s_user (id)
-);
--- ///
-
--- Tbl Pendaftar_Agen
-CREATE TABLE pendaftar_agen (
-  id       VARCHAR (36),
-  id_agen VARCHAR (36) NOT NULL,
-  id_pendaftar VARCHAR (36) NOT NULL,
-  tanggal TIMESTAMP NOT NULL,
-  status_tagihan VARCHAR (36)  NOT NULL,
-  PRIMARY KEY (id),
-  foreign key (id_pendaftar) references pendaftar (id),
-  foreign key (id_agen) references agen (id)
-);
--- ///
-
--- Tagihan Agen
-
-CREATE TABLE tagihan_agen (
-  id       VARCHAR (36),
-  id_agen VARCHAR (36) NOT NULL,
-  id_jenisbiaya VARCHAR (36) NOT NULL,
-  nomor_tagihan VARCHAR (255) NOT NULL,
-  tanggal_tagihan DATE NOT NULL,
-  keterangan VARCHAR (255) NOT NULL,
-  nilai NUMERIC (19,2) NOT NULL,
-  lunas BOOLEAN NOT NULL DEFAULT FALSE,
-  PRIMARY KEY (id),
-  foreign key (id_agen) references agen (id),
-  foreign key (id_jenisbiaya) references jenis_biaya (id)
-);
-
---////
-
--- Pembayaran Agen
-
-create table pembayaran_agen (
-  id VARCHAR (36),
-  id_tagihan VARCHAR(36) NOT NULL,
-  id_bank VARCHAR (36) NOT NULL,
-  waktu_pembayaran TIMESTAMP NOT NULL ,
-  cara_pembayaran VARCHAR (255) NOT NULL ,
-  bukti_pembayaran VARCHAR (255) NOT NULL ,
-  nilai NUMERIC(19,2) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (id_tagihan) REFERENCES tagihan_agen(id),
-  FOREIGN KEY (id_bank) REFERENCES bank(id)
 );
 
 -- ///
