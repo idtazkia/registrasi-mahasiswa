808a3f880ac799142f6a10e989c4ed3b1be84bee
Haffizh <haffizh@tazkia.ac.id>
2019-01-11 09:16:28 +0700

    update berkas & view berkas admin

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/UploadBerkasController.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/controller/UploadBerkasController.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -27,12 +26,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.ServletContext;
 import javax.validation.Valid;
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
@@ -223,9 +219,142 @@ public class UploadBerkasController {
     }
 
     @RequestMapping("/registrasi/berkas/hapus")
-    public  String hapus(@RequestParam("id") Berkas id ){
+    public  String hapus(@RequestParam("id") Berkas id, Authentication currentUser) throws Exception {
+
+        logger.debug("Authentication class : {}", currentUser.getClass().getName());
+
+        if (currentUser == null) {
+            logger.warn("Current user is null");
+        }
+
+        String username = ((UserDetails) currentUser.getPrincipal()).getUsername();
+        User u = userDao.findByUsername(username);
+        logger.debug("User ID : {}", u.getId());
+        logger.debug("ROLE ID : {}", u.getRole().getId());
+        if (u == null) {
+            logger.warn("Username {} not found in database ", username);
+        }
+
         berkasDao.delete(id);
-        return "redirect:/registrasi/berkas/list";
+
+        if (u.getRole().getId().equals("pendaftar")) {
+            return "redirect:/registrasi/berkas/list";
+        } else {
+            return "redirect:/registrasi/berkas/view?pendaftar="+ id.getPendaftar().getId();
+        }
     }
 
+//Form Akademik
+    @GetMapping("/registrasi/berkas/formAkademik")
+    public void berkasFormAkademik(@RequestParam Pendaftar pendaftar, Model model){
+        BerkasDto berkas = new BerkasDto();
+        model.addAttribute("berkas", berkas);
+        model.addAttribute("pendaftar", pendaftar);
+    }
+
+    @PostMapping("/registrasi/berkas/formAkademik")
+    public String  prosesUploadBerkasAkademik(@ModelAttribute @Valid BerkasDto berkasDto, BindingResult errors,
+                                      MultipartFile fileBerkas1,MultipartFile fileBerkas2,MultipartFile fileBerkas3
+            ,Pendaftar pendaftar) throws Exception{
+
+// persiapan lokasi upload
+
+
+        String idPeserta = pendaftar.getId();
+        String lokasiUpload = uploadFolder + File.separator + idPeserta;
+        logger.debug("Lokasi upload : {}", lokasiUpload);
+        new File(lokasiUpload).mkdirs();
+
+// Proses upload berkas.
+        simpanBerkas(fileBerkas1, pendaftar, berkasDto, lokasiUpload, berkasDto.getJenisBerkas1());
+        simpanBerkas(fileBerkas2, pendaftar, berkasDto, lokasiUpload, berkasDto.getJenisBerkas2());
+        simpanBerkas(fileBerkas3, pendaftar, berkasDto, lokasiUpload, berkasDto.getJenisBerkas3());
+
+
+        return "redirect:/registrasi/berkas/view?pendaftar="+ pendaftar.getId();
+    }
+
+//
+//Update Berkas
+
+    @RequestMapping(value = "/registrasi/berkas/update", method = RequestMethod.GET)
+    public String tampilkanForm(@RequestParam(value = "berkas", required = false) String berkas,
+                                Model m){
+        //defaultnya, isi dengan object baru
+        m.addAttribute("berkas", new Berkas());
+
+        if (berkas != null && !berkas.isEmpty()){
+            Berkas berkas1= berkasDao.findById(berkas).get();
+            if (berkas1 != null){
+                m.addAttribute("berkas", berkas1);
+            }
+        }
+        return "registrasi/berkas/update";
+    }
+
+    @RequestMapping(value = "/registrasi/berkas/update", method = RequestMethod.POST)
+    public String  prosesUpdateBerkas(@ModelAttribute @Valid Berkas berkas, BindingResult errors,
+                                      MultipartFile fileBerkas,String pendaftar,Authentication currentUser) throws Exception {
+
+        logger.debug("Authentication class : {}", currentUser.getClass().getName());
+
+        if (currentUser == null) {
+            logger.warn("Current user is null");
+        }
+
+        String username = ((UserDetails) currentUser.getPrincipal()).getUsername();
+        User u = userDao.findByUsername(username);
+        logger.debug("User ID : {}", u.getId());
+        logger.debug("ROLE ID : {}", u.getRole().getId());
+        if (u == null) {
+            logger.warn("Username {} not found in database ", username);
+        }
+
+        String idPeserta = pendaftar;
+
+        String namaFile = fileBerkas.getName();
+        String jenisFile = fileBerkas.getContentType();
+        String namaAsli = fileBerkas.getOriginalFilename();
+        Long ukuran = fileBerkas.getSize();
+
+        logger.debug("Nama File : {}", namaFile);
+        logger.debug("Jenis File : {}", jenisFile);
+        logger.debug("Nama Asli File : {}", namaAsli);
+        logger.debug("Ukuran File : {}", ukuran);
+
+//        memisahkan extensi
+        String extension = "";
+
+        int i = namaAsli.lastIndexOf('.');
+        int p = Math.max(namaAsli.lastIndexOf('/'), namaAsli.lastIndexOf('\\'));
+
+        if (i > p) {
+            extension = namaAsli.substring(i + 1);
+        }
+
+        String idFile = UUID.randomUUID().toString();
+        String lokasiUpload = uploadFolder + File.separator + idPeserta;
+        logger.debug("Lokasi upload : {}", lokasiUpload);
+        new File(lokasiUpload).mkdirs();
+        File tujuan = new File(lokasiUpload + File.separator + idFile + "." + extension);
+        berkas.setFileBerkas(idFile + "." + extension);
+        fileBerkas.transferTo(tujuan);
+        logger.debug("File sudah dicopy ke : {}", tujuan.getAbsolutePath());
+        logger.debug("Id Pendaftar : {}" , idPeserta);
+        berkasDao.save(berkas);
+
+        if (u.getRole().getId().equals("pendaftar")) {
+            return "redirect:/registrasi/berkas/list";
+        } else {
+            return "redirect:/registrasi/berkas/view?pendaftar="+ idPeserta;
+        }
+    }
+//////
+
+    @GetMapping("/registrasi/berkas/view")
+    public ModelMap viewBerkas(@RequestParam Pendaftar pendaftar, Pageable page){
+        ModelMap mm = new ModelMap();
+        mm.addAttribute("daftarBerkas",berkasDao.findByPendaftarOrderByJenisBerkas(pendaftar, page));
+        mm.addAttribute("pendaftar", pendaftar);
+        return mm;   }
 }
