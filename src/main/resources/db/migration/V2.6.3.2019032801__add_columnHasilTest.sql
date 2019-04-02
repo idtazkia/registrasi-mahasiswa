44cd2f5540c065fe288c31a67414b0c2fe409dd0
Haffizh <haffizh@tazkia.ac.id>
2019-04-02 08:26:08 +0700

    revisi skema

--- src/main/resources/db/migration/V2.6.3.2019032801__add_columnHasilTest.sql
+++ src/main/resources/db/migration/V2.6.3.2019032801__add_columnHasilTest.sql
@@ -1,5 +1,5 @@
 alter table hasil_test
-  add column user_insert varchar NOT NULL;
+  add column user_insert varchar;
 
 alter table hasil_test
   add column tanggal_insert timestamp without time zone;
