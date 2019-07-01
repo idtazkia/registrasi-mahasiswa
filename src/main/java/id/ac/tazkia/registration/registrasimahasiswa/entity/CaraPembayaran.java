ae09a375e494fd2dd2ffc1a2d1365ec881980867
Haffizh <haffizh@tazkia.ac.id>
2019-07-01 11:30:00 +0700

    tambah opsi pembayaran

--- src/main/java/id/ac/tazkia/registration/registrasimahasiswa/entity/CaraPembayaran.java
+++ src/main/java/id/ac/tazkia/registration/registrasimahasiswa/entity/CaraPembayaran.java
@@ -1,5 +1,5 @@
 package id.ac.tazkia.registration.registrasimahasiswa.entity;
 
 public enum CaraPembayaran {
-    TUNAI, TRANSFER, VIRTUAL_ACCOUNT
+    TUNAI, TRANSFER, VIRTUAL_ACCOUNT, BEASISWA
 }
